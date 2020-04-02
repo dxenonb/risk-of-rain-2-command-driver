e955002977ae8b7b292b9444813708a050cf39b2
dxb <drdxbme@gmail.com>
2020-04-01 22:08:11 -0400

    Add basic grid selector

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 pub mod robot;
 
-struct ItemPos(u32, u32);
+use std::collections::HashMap;
+use std::cmp;
+
+pub struct ItemPos(pub u32, pub u32);
 
 trait ItemEngine {
     fn select_item(&mut self, action: &Ror2Action) -> ItemPos;
@@ -17,4 +20,47 @@ enum Ror2Action {
 
 trait ActionDetector {
     fn read_screen(&mut self) -> Ror2Action;
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum ItemClass {
+    White,
+    Green,
+    Red,
+    Lunar,
+    UseItem,
+    BossItem,
+}
+
+/// Describes the ROR2 command chest UI and user's screen
+///
+/// We assume the placement of ROR2 does not change and game is maximized.
+pub struct ScreenInfo {
+    pub item_icon_size: u32,
+    pub item_icon_margin: u32,
+    pub screen_size: (u32, u32),
+    pub grid_size: HashMap<ItemClass, (u32, u32)>,
+}
+
+pub fn item_to_screen_pos(
+    screen: &ScreenInfo,
+    class: &ItemClass,
+    ItemPos(mut x, mut y): ItemPos
+) -> robot::MousePos {
+    let icon = screen.item_icon_size;
+    let half_icon = icon / 2;
+    let margin = screen.item_icon_margin;
+
+    let (grid_width, grid_height) = screen.grid_size.get(class).unwrap();
+    let grid_width = grid_width * icon + (grid_width - 1) * margin;
+    let grid_height = grid_height * icon + (grid_height - 1) * margin;
+    let offset = (grid_width / 2, grid_height / 2);
+
+    let (screen_width, screen_height) = screen.screen_size;
+    let min = (screen_width / 2 - offset.0, screen_height / 2 - offset.1);
+
+    x = half_icon + min.0 + (x * icon + (cmp::max(x as i32 - 1, 0) as u32) * margin);
+    y = half_icon + min.1 + (y * icon + (cmp::max(y as i32 - 1, 0) as u32) * margin);
+
+    robot::MousePos(x as _, y as _)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
 use ror2_command::robot;
 use ror2_command::robot::{Robot, MousePos};
+use ror2_command::{
+    ItemPos,
+    ItemClass,
+    ScreenInfo,
+    item_to_screen_pos,
+};
+
+use std::collections::HashMap;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() -> Result<(), ()> {
     let mut robot = robot::WinRobot::new();
-    robot.click_on(MousePos(500, 200));
-    robot.click_on(MousePos(500, 200));
+
+    let mut grid_size = HashMap::new();
+    grid_size.insert(ItemClass::White, (5, 5));
+    grid_size.insert(ItemClass::Red, (5, 4));
+
+    let screen = ScreenInfo {
+        item_icon_size: 76,
+        item_icon_margin: 6,
+        screen_size: (1920, 1080),
+        grid_size,
+    };
+
+    debug_mouse(&mut robot, &ItemClass::White, &screen);
+
     Ok(())
+}
+
+fn debug_mouse<R: Robot>(robot: &mut R, class: &ItemClass, screen: &ScreenInfo) {
+    let (width, height) = screen.grid_size.get(class).unwrap();
+
+    for y in 0..*height {
+        for x in 0..*width {
+            let pos = item_to_screen_pos(screen, class, ItemPos(x, y));
+            robot.mouse_to(pos);
+            sleep(Duration::from_millis(500));
+        }
+    }
 }
