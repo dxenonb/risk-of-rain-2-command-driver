4b38f626ace357c7d684f97a49c7c4ed497c7e01
dxb <drdxbme@gmail.com>
2020-04-04 22:26:07 -0400

    Get screen cap compiling (not working)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 pub mod robot;
 
+use winapi::shared::windef::{COLORREF, HBITMAP};
+
 use std::collections::HashMap;
 use std::cmp;
+use std::ptr;
 
 pub struct ItemPos(pub u32, pub u32);
 
@@ -63,4 +66,101 @@ pub fn item_to_screen_pos(
     y = half_icon + min.1 + (y * icon + (cmp::max(y as i32 - 1, 0) as u32) * margin);
 
     robot::MousePos(x as _, y as _)
+}
+
+pub fn screen_cap() {
+    use winapi::um::winuser::{
+        GetDC,
+        GetSystemMetrics,
+        ReleaseDC,
+        SM_XVIRTUALSCREEN,
+        SM_YVIRTUALSCREEN,
+        SM_CXVIRTUALSCREEN,
+        SM_CYVIRTUALSCREEN,
+    };
+    use winapi::um::wingdi::{
+        CreateCompatibleDC,
+        CreateCompatibleBitmap,
+        SelectObject,
+        BitBlt,
+        DeleteDC,
+        DeleteObject,
+        SRCCOPY,
+        CAPTUREBLT,
+    };
+    // Adapted from:
+    // https://stackoverflow.com/questions/3291167/how-can-i-take-a-screenshot-in-a-windows-application
+
+    unsafe {
+        // get screen info
+        let x  = GetSystemMetrics(SM_XVIRTUALSCREEN);
+        let y  = GetSystemMetrics(SM_YVIRTUALSCREEN);
+        let cx = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+        let cy = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+
+        // get a screen cap, save to bitmap
+        let dc_screen = GetDC(ptr::null_mut());
+        let dc_target = CreateCompatibleDC(dc_screen);
+        let bmp_target = CreateCompatibleBitmap(dc_screen, cx, cy);
+        if bmp_target == ptr::null_mut() {
+            panic!("Bitmap creation failed");
+        }
+        let old_bmp = SelectObject(dc_target, bmp_target as *mut _);
+        if BitBlt(dc_target, 0, 0, cx, cy, dc_screen, x, y, SRCCOPY | CAPTUREBLT) == 0 {
+            panic!("Bit blitting failed");
+        }
+
+        // process the bitmap ////
+        {
+            // get avg distance from target color across left side
+            let green = (118, 237, 34);
+            let avg = average_distance2(bmp_target, &green, 671, cy/2, 10);
+            println!("average: {:?}", avg);
+            // across the right side
+            // find the minimum
+            // verify they are similar
+        }
+        
+        SelectObject(dc_target, old_bmp);
+        DeleteDC(dc_target);
+        ReleaseDC(ptr::null_mut(), dc_screen);
+
+        // free the bitmap object
+        DeleteObject(bmp_target as *mut _);
+    }
+}
+
+unsafe fn average_distance2(
+    bitmap: HBITMAP,
+    target_color: &(i32, i32, i32),
+    mut x: i32,
+    y: i32,
+    span: i32,
+) -> i32 {
+    use winapi::um::wingdi::{GetPixel, CLR_INVALID};
+
+    let mut sum = 0;
+    for i in 0..span {
+        let color = GetPixel(bitmap as *mut _, x, y);
+        if color == CLR_INVALID {
+            panic!("Got invalid color for {}th pixel of span ({}, {}) \
+                - position must be out of bounds!", i, x, y);
+        }
+        sum += color_distance2(color, target_color);
+        x += 1;
+    }
+
+    sum / span
+}
+
+fn color_distance2(color: COLORREF, (r, g, b): &(i32, i32, i32)) -> i32 {
+    use winapi::um::wingdi::{GetRValue, GetGValue, GetBValue};
+
+    let src_red = GetRValue(color) as i32;
+    let src_green = GetGValue(color) as i32;
+    let src_blue = GetBValue(color) as i32;
+
+    (src_red - r).pow(2)
+        + (src_green - g).pow(2)
+        + (src_blue - b).pow(2)
 }
